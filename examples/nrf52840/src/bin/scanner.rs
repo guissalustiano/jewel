69edd83a06483195657502506ec10b6ad75bd687
Guilherme S. Salustiano <guissalustiano@gmail.com>
2024-01-25 01:25:36 +0100

    fix examples dependencies

--- examples/nrf52840/src/bin/scanner.rs
+++ examples/nrf52840/src/bin/scanner.rs
@@ -5,6 +5,7 @@ use defmt::{info, unwrap};
 use embassy_executor::Spawner;
 use embassy_nrf::{bind_interrupts, peripherals, radio};
 use embassy_time::Timer;
+use jewel::radio::BleRadio;
 use jewel::radio::MAX_PDU_LENGTH;
 use {defmt_rtt as _, panic_probe as _};
 
