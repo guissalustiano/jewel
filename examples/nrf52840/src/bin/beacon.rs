b3149001c617f5a3bace1fc70d93fbf0be26b425
Guilherme S. Salustiano <guissalustiano@gmail.com>
2024-02-07 00:14:56 +0100

    clear example

--- examples/nrf52840/src/bin/beacon.rs
+++ examples/nrf52840/src/bin/beacon.rs
@@ -5,23 +5,18 @@ use defmt::info;
 use embassy_executor::Spawner;
 use embassy_nrf::{bind_interrupts, peripherals, radio};
 use embassy_time::Duration;
-use jewel::{phy::MAX_PDU_LENGTH, Address, AdvData, Broadcaster, Flags};
+use jewel::{Address, AdvData, Broadcaster, Flags};
 use {defmt_rtt as _, panic_probe as _};
 
 bind_interrupts!(struct Irqs {
     RADIO => radio::InterruptHandler<peripherals::RADIO>;
 });
 
-// Same payload as the embassy/nrf-softdevice ble_advertising example,
-// but just in channel 39.
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let mut config = embassy_nrf::config::Config::default();
     config.hfclk_source = embassy_nrf::config::HfclkSource::ExternalXtal;
     let p = embassy_nrf::init(config);
-
-    // I don't know how not to depend on the buffer allocation at the beginning
-    let mut buffer = [0u8; MAX_PDU_LENGTH];
 
     info!("Starting BLE radio");
     let mut radio = radio::ble::Radio::new(p.RADIO, Irqs);
@@ -33,7 +28,6 @@ async fn main(_spawner: Spawner) {
             .set_flags(Flags::discoverable())
             .set_uuids16(&[0x0918])
             .set_complete_local_name("HelloRust"),
-        &mut buffer,
     )
     .unwrap();
 
